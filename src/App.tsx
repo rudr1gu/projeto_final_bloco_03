8d36c087362127900eead3fb6a7a99b7b696f15e
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-26 19:43:12 -0300

    :sparkles: feat: implement routing with react-router-dom in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
@@ -8,10 +9,17 @@ function App() {
 
   return (
     <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <main className="min-h-[80vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </BrowserRouter>
+</>
   )
 }
 

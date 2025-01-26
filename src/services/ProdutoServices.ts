006e129840b07943891d0736874101cef6e14be3
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-26 20:27:43 -0300

    :sparkles: feat: add createProduto method to ProdutoServices for product creation

--- src/services/ProdutoServices.ts
+++ src/services/ProdutoServices.ts
@@ -1,8 +1,14 @@
+import Produto from "../models/Produto";
 import baseApi from "./baseApi";
 
 class ProdutoServices {
     getAllProdutos = async (setDados: Function) => {
         const response = await baseApi.get("/produtos");
+        setDados(response.data);
+    }
+
+    createProduto = async (produto: Produto, setDados: Function) => {
+        const response = await baseApi.post("/produtos", produto);
         setDados(response.data);
     }
 }

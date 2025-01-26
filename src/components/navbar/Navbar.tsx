363fb0d6c6039bd8f3cf20afa24f55574ed094c6
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-26 19:50:14 -0300

    :art: style: update Navbar component styling and simplify navigation links

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,30 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar =() =>{
     return(
-        <nav className="w-full flex justify-center py-4 bg-sky-700 text-white">
+        <nav className="w-full flex justify-center py-4 bg-sky-900 text-white">
         <div className="container flex justify-between text-lg">
-            {/* <h1><Link to="/home">Farmacia </Link> <i className='bx bx-code-alt'></i></h1> */}
+            <h1><Link to="/home">Farmacia </Link> <i className='bx bxs-band-aid'></i></h1>
             <div>
                 <ul className="flex gap-5">
-                    {/* <li><Link to="/postagens" className="links-nav">
-                        <i className='bx bxs-notepad'></i> Postagens
-                    </Link>
-                    </li>
-                    <li><Link to="/tema" className="links-nav ">
-                        <i className='bx bxs-comment-detail'></i> Temas
-                    </Link>
-                    </li>
-                    <li><Link to="cadastrartema" className="links-nav ">
-                        <i className='bx bxs-comment-add'></i> Cadastrar Tema
-                    </Link>
-                    </li>
-                    <li><Link to="/perfil" className="links-nav ">
-                        <i className='bx bxs-user-rectangle' ></i> Perfil
-                    </Link>
-                    </li>
-                    <li><Link to='' onClick={logout} className="links-nav logout">
-                        <i className='bx bxs-log-out-circle'></i> Sair
-                    </Link>
-                    </li> */}
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/home">Produtos</Link></li>
+                    <li><Link to="/home">Contato</Link></li>
                 </ul>
             </div>
         </div>

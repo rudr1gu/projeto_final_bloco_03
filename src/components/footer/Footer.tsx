c6bb1b95c6f4849725c65553a87ac9b83c74a42d
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-01-26 19:37:10 -0300

    :sparkles: feat: update Footer component to include current year in copyright notice

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
+    const data = new Date().getFullYear();
 
     return (
         <footer className="flex justify-center bg-sky-900 text-white">
             <div className="container flex flex-col items-center py-4">
                 <div className="text-xl font-bold">
                     <p>
-                       Desenvolvido por: Rudr1gu
+                       Desenvolvido por: Rudr1gu | copyrigth {data}
                     </p>
-                    <p className='text-lg'>Acesse minhas redes sociais</p>
                     <div className='flex gap-2 justify-center'>
                         <a href="https://instagram.com/rudr1gu" className=""><i className='bx bxl-instagram-alt'></i></a>
                         <a href="https://github.com/rudr1gu"><i className='bx bxl-github'></i></a>
